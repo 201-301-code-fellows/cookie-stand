30c4fbad6e0aa6803f680d517592ee9514cbebfd
Lewis Benson <tm.lewis.benson@gmail.com>
2022-09-20 17:46:16 -0400

    fully working build with constructors, methods to render, and refactored to meet requirements

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const hoursOpen = 14; /* 6AM to 7PM totals to 14 hours */
 const timeOpenArray = [
   'Time:',
   '6am:',
@@ -18,7 +17,7 @@ const timeOpenArray = [
   '6pm:',
   '7pm:',
 ];
-
+/* Arguments to be used when invocating City constructor */
 const cityArgs = [
   ['Seattle', 23, 65, 6.3],
   ['Tokyo', 3, 24, 1.2],
@@ -27,13 +26,16 @@ const cityArgs = [
   ['Lima', 2, 16, 4.6],
 ];
 
+/* Select the table element from HTML to build the table on */
+const table = document.querySelector('table');
+
 /* Utility Functions */
 // Used MDN for template
-function getRandomInt(min, max) {
+function randInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-/* This array is where all data is pulled from for all functions, if a objects(locations) are added or removed, edit this array */
+/* This array is where all data is pulled from for all functions to */
 const locations = [];
 
 /* Constructor for cties */
@@ -42,18 +44,29 @@ function City(cityName, minCust, maxCust, avgSales) {
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgSales = avgSales;
-  this.results = [];
+  this.results = [this.cityName];
   this.totalSales = 0;
   locations.push(this);
 }
 
 City.prototype.custPerHour = function () {
-  return getRandomInt(this.minCust, this.maxCust);
+  for (let i = 0; i < timeOpenArray.length - 1; i++) {
+    const num = Math.floor(this.avgSales * randInt(this.minCust, this.maxCust));
+    this.totalSales += num;
+    this.results.push(num);
+  }
 };
 
 City.prototype.render = function () {
-  //TODO appendChild can go here
-  //TODO building the TR and TDs can go here
+  const tableRow = document.createElement('tr');
+  table.appendChild(tableRow);
+  let tableData = document.createElement('td');
+  // tableRow.class = this.cityName.toLowerCase();
+  for (let data of this.results) {
+    tableData = document.createElement('td');
+    tableData.innerText = data;
+    tableRow.appendChild(tableData);
+  }
 };
 
 /* Generate new City objects that contain the data in cityArgs array */
@@ -61,26 +74,19 @@ for (let data of cityArgs) {
   new City(...data);
 }
 
-/* Generate objects for each location */
-buildResults(locations);
-
-/* Build data for each location based on min/max values in objects */
-function buildResults(location) {
-  for (let city of location) {
-    for (let i = 0; i < hoursOpen; i++) {
-      city.results.push(city.custPerHour() * Math.floor(city.avgSales));
-      city.totalSales += city.results[i];
-    }
-  }
-}
-
-const table = document.querySelector('table');
-
 function createHeading() {
   for (let time of timeOpenArray) {
     const tableHead = document.createElement('th');
-    tableHead.innerText = `${time}  `;
+    tableHead.innerText = `${time}`;
     table.appendChild(tableHead);
   }
 }
-createHeading();
+
+function buildThePage() {
+  createHeading();
+  for (let city of locations) {
+    city.custPerHour();
+    city.render();
+  }
+}
+buildThePage();
